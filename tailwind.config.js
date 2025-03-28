64ce35dc5636c2e7926c793381e1edadbe019da8
lookino123 <l.demichelis@gmail.com>
2025-03-28 15:32:58 -0400

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,33 @@
-/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     container: {
       center: true,
       padding: '1rem',
-  },
-  screens: {
+    },
+    screens: {
       sm: '640px',
       md: '768px',
       lg: '1024px',
       xl: '1280px',
-  },
-  fontFamily: {
-     manrope: ['Manrope', 'sans-serif'],
-  },
-  colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      white: '#ffffff',
-      black: '#08111F',
-      primary: '#006BFD',
-      secondary: '#ea47ed', 
-      gray: {
-        lighter: '#FAF7F3',
-        light: '#323232', 
-        dark: '#1C2C3C',    
-        txt: '#252800',
+    },
+    fontFamily: {
+      manrope: ['Manrope', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        primary: '#006BFD',
+        secondary: '#ea47ed',
+        gray: {
+          lighter: '#FAF7F3',
+          light: '#323232',
+          dark: '#1C2C3C',
+          txt: '#252800',
+        },
       },
-  },
-    extend: {},
+    },
   },
   plugins: [],
 }
-
