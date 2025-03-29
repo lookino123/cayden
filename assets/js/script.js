dd7ff85658ca047a480bf51a5896f9a833384017
lookino123 <l.demichelis@gmail.com>
2025-03-29 10:19:30 -0400

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,21 +52,6 @@ startCountersOnScroll();
 
 
 
-new Swiper('.swiper', {
-  slidesPerView: 1,
-  loop: true,
-  autoplay: {                       
-      delay: 3000,  
-  },
-  pagination: {                      
-      el: '.swiper-pagination',
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
-
 
 document.addEventListener('DOMContentLoaded', function() {
   const filterButtons = document.querySelectorAll('.filter-button');
