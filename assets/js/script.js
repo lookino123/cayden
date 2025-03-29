249e3b028ca3f246bbed46753563a5161cb3ce63
lookino123 <l.demichelis@gmail.com>
2025-03-29 11:14:34 -0400

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,4 +48,28 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+
+      const targetId = this.getAttribute('href');
+      const targetSection = document.querySelector(targetId);
+      if (targetSection) {
+          const offset = 88;
+          const targetPosition = targetSection.getBoundingClientRect().top + window.scrollY - offset;
+
+
+          window.scrollTo({
+              top: targetPosition,
+              behavior: 'smooth'
+          });
+      }
+  });
+});
+
+
+
+
+
 
